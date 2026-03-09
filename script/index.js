aec1c2be1f1012e4ab871dc8decf6e62efa53b66
asm-ayesha <ayesha219333@gmail.com>
2026-03-09 09:59:56 +0600

    fatch all issues api and show disylay

--- script/index.js
+++ script/index.js
@@ -28,4 +28,49 @@ async function loadIssues() {
     const data = await res.json()
 
     console.log(data.data)
+    displayIssues(data.data)
 }
+
+
+function displayIssues(issues){
+    const container = document.getElementById("issues-container")
+    container.innerHTML = ""
+
+    issues.forEach(issue => { 
+        const div = document.createElement("div")
+        div.innerHTML = ` 
+        <div class="border-t-4  border-amber-200  rounded-lg shadow-lg h-full">
+                <div class="p-8">
+                    <div class="flex justify-between mb-4">
+                        <img src="./assets/Open-Status.png" alt="">
+                        <p class="px-4  bg-red-100 text-center text-red-500 font-bold rounded-4xl">${issue.priority}</p>
+                    </div>
+
+                    <h2 class="text-xl font-bold mb-2">${issue.title}</h2>
+                    <p class="text-gray-500 line-clamp-2">${issue.description}</p>
+
+                    <div class="flex justify-between mt-4">
+                        <p class="px-4 py-2 bg-red-100 text-center text-red-500 font-bold rounded-4xl"><i
+                                class="fa-solid fa-bug"></i> Bug</p>
+                        <p class="px-4 py-2 bg-amber-100 text-center text-amber-500 font-bold rounded-4xl"><i
+                                class="fa-solid fa-life-ring"></i> help wanted</p>
+                    </div>
+
+                </div>
+
+                <hr class="border border-gray-300 mb-4">
+
+                <div class="pl-8 pb-8  space-y-4"> 
+                    <p class="text-gray-500">#${issue.id} by ${issue.author}</p>
+                    <p class="text-gray-500">${issue.createdAt}</p>
+                </div>
+
+            </div>
+        
+        `
+
+         container.appendChild(div);
+    })
+}
+
+loadIssues()
